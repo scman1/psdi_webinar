0a38df4b6bfaeb48bdd29f98e022d87f81b461b7
Abraham Nieva de la Hidalga <a_nieva@hotmail.com>
2023-09-28 09:14:08 +0100

    path selection example

--- lib/custom_plots.py
+++ lib/custom_plots.py
@@ -1,3 +1,5 @@
+import manage_athena as athenamgr
+
 from matplotlib import pyplot as plt
 import matplotlib.cm as cm
 
@@ -114,5 +116,37 @@ def plot_circles(data_set,rmin,rmax,kmin,kmax, datalabel="data"):
     ax2.set_xlabel("$R(\mathrm{\AA})$")
     ax2.set_ylabel("$|\chi(R)|(\mathrm{\AA}^{-3})$")
     ax2.legend(loc='upper right')
+
+    return plt
+
+def get_groups(data_file, data_mappings):
+    data_prj = athenamgr.read_project(data_file)
+    data_groups = {}
+    for a_mapping in data_mappings:
+        data_groups[a_mapping] = athenamgr.calc_with_defaults(athenamgr.get_group(data_prj, data_mappings[a_mapping]))
+        data_groups[a_mapping].filename =  a_mapping
+    return data_groups
+
+# plot normalised spectrum wir custom colors and line styles
+def plot_normalised(athena_groups = {}, include_groups = {}, aspect = (6,8), xlim=[],ylim=[]):
+    plt.figure(figsize=aspect)
+    for g_indx, a_group in enumerate(include_groups):
+        if athena_groups[a_group].filename in include_groups:
+                plt.plot(athena_groups[a_group].energy, 
+                         athena_groups[a_group].norm, 
+                         label=athena_groups[a_group].filename,
+                         color = include_groups[a_group][0],
+                         linestyle = include_groups[a_group][1]
+                        ) 
+
+    frame1 = plt.gca()
+    #frame1.axes.yaxis.set_ticklabels([])
+    #frame1.axes.yaxis.set_ticklabels([])
+    plt.ylabel("Normalized XANES (a.u.)")
+    plt.xlabel("Energy (eV)")
+    plt.xlim(xlim)
+    plt.ylim(ylim)
+    plt.legend()
+    plt.show()
 
     return plt
